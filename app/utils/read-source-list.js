2f26bcfcf1a5bc2553b43df498865ec298718af1
ruby <mxstr73@gmail.com>
2024-10-10 14:07:54 +0300

    upd structure

--- app/utils/read-source-list.js
+++ app/utils/read-source-list.js
@@ -2,7 +2,7 @@ import { resolve } from 'node:path';
 import { readFileSync } from 'node:fs';
 
 export const readSourceList = (name) => {
-  const path = resolve(process.cwd() + `/output/${name}.list`);
+  const path = resolve(process.cwd() + `/sources/${name}.list`);
 
   return readFileSync(path).toString().replaceAll('\r', '').split('\n').filter(source => !!source);
 }
--- app/utils/save-output.js
+++ app/utils/save-output.js
@@ -2,7 +2,7 @@ import { resolve } from 'node:path';
 import { writeFileSync } from 'node:fs';
 
 export const saveOutput = (output) => {
-  const path = resolve(process.cwd() + `/output/output.json`);
+  const path = resolve(process.cwd() + `/output/amnezia_sites.json`);
 
   return writeFileSync(path, JSON.stringify(output, null, 2));
 }
